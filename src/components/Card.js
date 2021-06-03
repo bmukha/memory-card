0be2c17023ef320e38faf73ca35040eda952eff1
bmukha <47007157+bmukha@users.noreply.github.com>
2021-06-03 14:17:42 +0300

    next stage of styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import "../styles/Card.css";
 const Card = (props) => {
   console.log("Received props: ", props);
   return (
-    <div className="card">
+    <div className="card" style={{ boxShadow: `10px 10px 10px ${props.colors.hex}`}}>
       <div style={{ backgroundColor: props.colors.hex }}></div>
       <h2>{props.colors.name}</h2>
     </div>
